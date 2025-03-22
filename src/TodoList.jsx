7959a7c6688dea0fc126f973b5cfebeff122166b
rakeshullengula56 <ramullengularakesh@gmail.com>
2025-03-22 20:09:33 +0530

    Added css to TodoList component and it was funny

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import './TodoList.css'; // Import the CSS file
 
 function TodoList() {
     let [todo, setTodo] = useState([{ id: uuidv4(), task: "Wake up at 7PM", isDone: false }]);
@@ -44,30 +45,40 @@ function TodoList() {
     };
 
     return (
-        <div>
+        <div className="todo-container">
             <input
                 onChange={handleOnChange}
                 placeholder="Add a task"
                 value={newTodo}
                 type="text"
+                className="todo-input"
             />
             <br />
-            <button onClick={addNewTask}>Add task</button>
+            <button onClick={addNewTask} className="todo-button">Add Task</button>
             <br />
             <h3>Task List</h3>
-            <ul>
+            <ul className="todo-list">
                 {todo.map((item) => (
-                    <li key={item.id} style={{ textDecoration: item.isDone ? "line-through" : "none" }}>
+                    <li key={item.id} className={`todo-item ${item.isDone ? 'done' : ''}`}>
                         <span>{item.task}</span>
-                        &nbsp;&nbsp;&nbsp;
-                        <button onClick={() => updateTask(item.id)}>Mark as Done</button>
-                        <button onClick={() => deleteTask(item.id)}>Delete</button>
+                        {item.isDone && <span> ✅</span>}
+                        <div className="todo-actions">
+                            {!item.isDone && (
+                                <button onClick={() => updateTask(item.id)} className="todo-button todo-done">
+                                    Mark as Done
+                                </button>
+                            )}
+                            <button onClick={() => deleteTask(item.id)} className="todo-button todo-delete">
+                                Delete
+                            </button>
+                        </div>
                     </li>
                 ))}
             </ul>
-            <button onClick={upperCaseAll}>Mark All as Done</button>
+            <button onClick={upperCaseAll} className="todo-button">Mark All as Done</button>
         </div>
     );
 }
 
 export default TodoList;
+
